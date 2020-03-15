06f7305a94b781fa0bb578fdadc534bf77dd7fdd
gandrespulido <56008629+gandrespulido@users.noreply.github.com>
2020-03-15 01:56:49 -0500

    Update slimapp.sql

--- sql/slimapp.sql
+++ sql/slimapp.sql
@@ -23,47 +23,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `estudiantes`
 --
 
-CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `state` varchar(255) NOT NULL
+CREATE TABLE `estudiantes` (
+  `codigo` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `telefono` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `ciudad` varchar(255) NOT NULL,
+  `carrera` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customers`
+-- Dumping data for table `estudiantes`
 --
 
-INSERT INTO `customers` (`id`, `first_name`, `last_name`, `phone`, `email`, `address`, `city`, `state`) VALUES
-(2, 'Sam', 'Smith', '333-333-3333', 'ssmith@yahoo.com', '33 Birch Rd', 'Miami', 'FL'),
-(3, 'Brad', 'Traversy', '333-333-3333', 'brad@test.com', '333 South st', 'Portland', 'ME');
+INSERT INTO `estudiantes` (`codigo`, `nombre`, `apellido`, `telefono`, `correo`, `direccion`, `ciudad`, `departamento`) VALUES
+(2, 'German', 'Pulido', '1234567', 'gpulido@hotmail.com', 'cll 76 # 107 33', 'Bogota', 'Sistemas'),
+(3, 'Karen', 'Gonzalez', '4567890', 'kgonzalez@hotmail.com', 'cll 6 # 10 37', 'Mosquera', 'Sistemas'),
+(4, 'Tatiana', 'Muñoz', '7891234', 'tmuñoz@hotmail.com', 'cll 176 # 17 33', 'Bogota', 'Diseño grafico'),
+(5, 'Sara', 'muñoz', '7412589', 'smuñoz@hotmail.com', 'cll 176 # 17 33', 'Bogota', 'Diseño grafico'),
+(6, 'Ryu', 'Pulido', '9637415', 'rpulido@hotmail.com', 'cll 76 # 107 33', 'Bogota', 'Sistemas'),
+(7, 'Camila', 'Alvarado', '9517532', 'calvarado@hotmail.com', 'cll 106 # 7 23', 'Bogota', 'Medicina'),
+(8, 'jhonatan', 'villanueva', '8754265', 'jvillanueva@hotmail.com', 'cll 96 # 77 13', 'Bogota', 'Medicina'),
+(9, 'Angie', 'trujillo', '8954127', 'atrujillo@hotmail.com', 'cll 111 # 57 03', 'Bogota', 'Arquitectura'),
+(10, 'Edilma', 'trujillo', '5489545', 'etrujillo@hotmail.com', 'cll 111 # 57 03', 'Bogota', 'Arquitectura');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customers`
+-- Indexes for table `estudiantes`
 --
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`codigo`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT for table `estudiantes`
 --
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `estudiantes`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
